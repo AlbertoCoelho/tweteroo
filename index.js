2d374c5e86ae00cde4759b480986d85fc6333446
Alberto <tejotaesi@gmail.com>
2022-04-23 13:58:30 -0300

    Creating server routes

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const server = express();
 server.use(json());
 server.use(cors());
 
+const user = [];
+const tweet = [];
+
+server.post("/sign-up", (req,res) => {
+  res.send("post");
+})
+
+server.post("/tweets", (req,res) => {
+  res.send("post");
+});
+
+server.get("/tweets", (req,res) => {
+  res.send("get");
+});
 
 
 
